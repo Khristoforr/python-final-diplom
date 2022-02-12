120d69ca5fca93ec128a814c1981058e064c5e1b
Evgenii Khristoforov <khristoforover@gmail.com>
2022-02-12 15:13:51 +0500

    Advanced stage. Added simple throttling.

--- orders/orders/settings.py
+++ orders/orders/settings.py
@@ -118,8 +118,17 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend',
         'rest_framework.filters.SearchFilter',
-    ]
+    ],
 
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ],
+
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '5/minute',
+        'user': '1000/day'
+    }
 }
 
 # Internationalization
